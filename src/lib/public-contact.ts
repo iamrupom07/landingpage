47081ba6d379996bc077e70a58e206a5e4d50da4
Rupom <saidurrahmanrupom@gmail.com>
2026-06-02 21:05:49 +0600

    new code

--- src/lib/public-contact.ts
+++ src/lib/public-contact.ts
@@ -1,8 +1,8 @@
 export const publicContact = {
   address: "129 E Highland Ave, Monroe, GA",
   phone: {
-    display: "678-635-2455",
-    href: "tel:+16786352455",
+    display: "678-635-2445",
+    href: "tel:+16786352445",
   },
   email: {
     display: "pascal.bayonne@uniti.com",
